cfbd058e9c3837c6ee2d4f3dc45206cf30957895
David Garcia <empoalp@gmail.com>
2023-09-25 11:41:07 +0200

    Vector lottie

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Lottie from "lottie-react";
 import './App.css'
-import animationData from "./Prueba_engranajes.json";
+//import animationData from "./Prueba_engranajes.json";
+import animationData from "./assets/Prueba_engranajes_capavectorial.json";
 
 function App() {
   return (
